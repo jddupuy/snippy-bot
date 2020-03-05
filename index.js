603e54b7bfcb767024f4fef928b2a7b6b7d1814e
Patrick Sison <patrick.m.sison@gmail.com>
2020-03-05 16:01:05 -0500

    Added the rest of the server

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config/default.json');
+const _ = require('lodash');
 
-console.log(config);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-// client.on('ready', () => {
-//   console.log(`Logged in as ${client.user.tag}!`);
-// });
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+    console.log('Replied');
+  }
+});
 
-// client.on('message', msg => {
-//   if (msg.content === 'ping') {
-//     msg.reply('Pong!');
-//   }
-// });
-
-// client.login('token');
+client.login(_.get(config, 'discord.secret'));
